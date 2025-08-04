74c1a9fbe427a610655f871296add70377041810
Furkan Türker <furkanturker.dev@gmail.com>
2025-08-04 21:25:34 +0300

    Katmanlar oluşturuldu. Entityler tamamlandı.

---
+++ Entity/Concrate/News.cs
@@ -0,0 +1,10 @@
+namespace Entity.Concrate;
+
+public class News
+{
+    public int NewsId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public bool Status { get; set; }
+}
---
+++ Entity/Concrate/Service.cs
@@ -0,0 +1,9 @@
+namespace Entity.Concrate;
+
+public class Service
+{
+    public int ServiceId { get; set; }
+    public string Titlr { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+}
