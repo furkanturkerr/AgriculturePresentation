038b6e0733863a0f2eb8aff3a8f944ea2ba44bab
Furkan Türker <furkanturker.dev@gmail.com>
2025-08-16 01:40:55 +0300

    Toparlama

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c888bf7a390c415fd7fa35b505456a4cd161e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2d806bb4e26adb3cff7f70f5f4efa3c2629710")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
