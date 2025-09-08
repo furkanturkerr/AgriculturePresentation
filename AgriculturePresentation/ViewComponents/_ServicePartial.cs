93a598586265f1467f5dccbd2e21ddf484f4d71c
Furkan Türker <furkanturker.dev@gmail.com>
2025-09-08 22:52:29 +0300

    Service İşlemleri

---
+++ AgriculturePresentation/ViewComponents/_ServicePartial.cs
@@ -0,0 +1,19 @@
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents;
+
+public class _ServicePartial:ViewComponent
+{
+    private readonly IServiceService _serviceService;
+    public _ServicePartial(IServiceService serviceService)
+    {
+        _serviceService = serviceService;
+    }
+    
+    public IViewComponentResult Invoke()
+    {
+        var values = _serviceService.GetAll();
+        return View(values);
+    }
+}
--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c425f20078ca4c9bb29cc9ce7ca17965dc213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5fbcd8f6171160b5ef1c8fddea421dd014cf9ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
