06701fe27aa3b5f47cd226ffbd013e35d02e82e1
Furkan Türker <furkanturker.dev@gmail.com>
2025-09-11 00:22:43 +0300

    Adres

---
+++ AgriculturePresentation/ViewComponents/_AddressPartial.cs
@@ -0,0 +1,18 @@
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents;
+
+public class _AddressPartial:ViewComponent
+{
+    private readonly IAddressService _addressService;
+    public _AddressPartial(IAddressService addressService)
+    {
+        _addressService = addressService;
+    }
+    public IViewComponentResult Invoke()
+    {
+        var values = _addressService.GetAll();
+        return View(values);
+    }
+}
