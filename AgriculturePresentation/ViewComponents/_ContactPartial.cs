5bb8b6673fbf879e17501ca05beacd818a32ad3e
Furkan Türker <furkanturker.dev@gmail.com>
2025-09-11 00:27:28 +0300

    İletişim Formu

---
+++ AgriculturePresentation/ViewComponents/_ContactPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents;
+
+public class _ContactPartial:ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
